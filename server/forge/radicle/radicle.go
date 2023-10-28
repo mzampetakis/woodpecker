bf1802eae28fce1ae591bb2aafcdd4d475dea94b
Michalis Zampetakis <mzampetakis@gmail.com>
2023-10-28 08:42:25 +0300

    Refactor radicle as non exported

--- server/forge/radicle/radicle.go
+++ server/forge/radicle/radicle.go
@@ -14,8 +14,8 @@ type Opts struct {
 	SecretToken string // Radicle secret token.
 }
 
-// Radicle implements "Forge" interface
-type Radicle struct {
+// radicle implements "Forge" interface
+type radicle struct {
 	url         string
 	secretToken string
 }
@@ -23,130 +23,130 @@ type Radicle struct {
 // New returns a new forge Configuration for integrating with the Radicle
 // repository hosting service at https://radicle.xyz
 func New(opts Opts) (forge.Forge, error) {
-	return &Radicle{
+	return &radicle{
 		url:         opts.URL,
 		secretToken: opts.SecretToken,
 	}, nil
 }
 
 // Name returns the string name of this driver
-func (rad Radicle) Name() string {
+func (rad radicle) Name() string {
 	//TODO implement me
 	panic("implement me")
 }
 
 // URL returns the root url of a configured forge
-func (rad Radicle) URL() string {
+func (rad radicle) URL() string {
 	//TODO implement me
 	panic("implement me")
 }
 
 // Login authenticates the session and returns the
 // forge user details.
-func (rad Radicle) Login(ctx context.Context, w http.ResponseWriter, r *http.Request) (*model.User, error) {
+func (rad radicle) Login(ctx context.Context, w http.ResponseWriter, r *http.Request) (*model.User, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
 // Auth authenticates the session and returns the forge user
 // login for the given token and secret
-func (rad Radicle) Auth(ctx context.Context, token, secret string) (string, error) {
+func (rad radicle) Auth(ctx context.Context, token, secret string) (string, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
 // Teams fetches a list of team memberships from the forge.
-func (rad Radicle) Teams(ctx context.Context, u *model.User) ([]*model.Team, error) {
+func (rad radicle) Teams(ctx context.Context, u *model.User) ([]*model.Team, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
 // Repo fetches the repository from the forge, preferred is using the ID, fallback is owner/name.
-func (rad Radicle) Repo(ctx context.Context, u *model.User, remoteID model.ForgeRemoteID, owner, name string) (*model.Repo, error) {
+func (rad radicle) Repo(ctx context.Context, u *model.User, remoteID model.ForgeRemoteID, owner, name string) (*model.Repo, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
 // Repos fetches a list of repos from the forge.
-func (rad Radicle) Repos(ctx context.Context, u *model.User) ([]*model.Repo, error) {
+func (rad radicle) Repos(ctx context.Context, u *model.User) ([]*model.Repo, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
 // File fetches a file from the forge repository and returns it in string
 // format.
-func (rad Radicle) File(ctx context.Context, u *model.User, r *model.Repo, b *model.Pipeline, f string) ([]byte, error) {
+func (rad radicle) File(ctx context.Context, u *model.User, r *model.Repo, b *model.Pipeline, f string) ([]byte, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
 // Dir fetches a folder from the forge repository
-func (rad Radicle) Dir(ctx context.Context, u *model.User, r *model.Repo, b *model.Pipeline, f string) ([]*forge_types.FileMeta, error) {
+func (rad radicle) Dir(ctx context.Context, u *model.User, r *model.Repo, b *model.Pipeline, f string) ([]*forge_types.FileMeta, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
 // Status sends the commit status to the forge.
 // An example would be the GitHub pull request status.
-func (rad Radicle) Status(ctx context.Context, u *model.User, r *model.Repo, b *model.Pipeline, p *model.Workflow) error {
+func (rad radicle) Status(ctx context.Context, u *model.User, r *model.Repo, b *model.Pipeline, p *model.Workflow) error {
 	//TODO implement me
 	panic("implement me")
 }
 
 // Netrc returns a .netrc file that can be used to clone
 // private repositories from a forge.
-func (rad Radicle) Netrc(u *model.User, r *model.Repo) (*model.Netrc, error) {
+func (rad radicle) Netrc(u *model.User, r *model.Repo) (*model.Netrc, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
 // Activate activates a repository by creating the post-commit hook.
-func (rad Radicle) Activate(ctx context.Context, u *model.User, r *model.Repo, link string) error {
+func (rad radicle) Activate(ctx context.Context, u *model.User, r *model.Repo, link string) error {
 	//TODO implement me
 	panic("implement me")
 }
 
 // Deactivate deactivates a repository by removing all previously created
 // post-commit hooks matching the given link.
-func (rad Radicle) Deactivate(ctx context.Context, u *model.User, r *model.Repo, link string) error {
+func (rad radicle) Deactivate(ctx context.Context, u *model.User, r *model.Repo, link string) error {
 	//TODO implement me
 	panic("implement me")
 }
 
 // Branches returns the names of all branches for the named repository.
-func (rad Radicle) Branches(ctx context.Context, u *model.User, r *model.Repo, p *model.ListOptions) ([]string, error) {
+func (rad radicle) Branches(ctx context.Context, u *model.User, r *model.Repo, p *model.ListOptions) ([]string, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
 // BranchHead returns the sha of the head (latest commit) of the specified branch
-func (rad Radicle) BranchHead(ctx context.Context, u *model.User, r *model.Repo, branch string) (string, error) {
+func (rad radicle) BranchHead(ctx context.Context, u *model.User, r *model.Repo, branch string) (string, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
 // PullRequests returns all pull requests for the named repository.
-func (rad Radicle) PullRequests(ctx context.Context, u *model.User, r *model.Repo, p *model.ListOptions) ([]*model.PullRequest, error) {
+func (rad radicle) PullRequests(ctx context.Context, u *model.User, r *model.Repo, p *model.ListOptions) ([]*model.PullRequest, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
 // Hook parses the post-commit hook from the Request body and returns the
 // required data in a standard format.
-func (rad Radicle) Hook(ctx context.Context, r *http.Request) (repo *model.Repo, pipeline *model.Pipeline, err error) {
+func (rad radicle) Hook(ctx context.Context, r *http.Request) (repo *model.Repo, pipeline *model.Pipeline, err error) {
 	//TODO implement me
 	panic("implement me")
 }
 
 // OrgMembership returns if user is member of organization and if user
 // is admin/owner in that organization.
-func (rad Radicle) OrgMembership(ctx context.Context, u *model.User, org string) (*model.OrgPerm, error) {
+func (rad radicle) OrgMembership(ctx context.Context, u *model.User, org string) (*model.OrgPerm, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
 // Org fetches the organization from the forge by name. If the name is a user an org with type user is returned.
-func (rad Radicle) Org(ctx context.Context, u *model.User, org string) (*model.Org, error) {
+func (rad radicle) Org(ctx context.Context, u *model.User, org string) (*model.Org, error) {
 	//TODO implement me
 	panic("implement me")
 }
